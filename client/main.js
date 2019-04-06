0b62e8eba024e0d9def1768aedc6b0dbb697def8
Aj Gill <ajgill@ucdavis.edu>
2019-04-06 15:29:54 -0700

    DB link + buttons
    Implemented arrow functions, added buttons for removing players, subtracting & adding the score

--- client/main.js
+++ client/main.js
@@ -1,22 +1,54 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
 
-import './main.html';
+import {Players} from './../imports/api/players';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const renderPlayers = (playersList) => {
+  return playersList.map((player) => {
+    return (
+      <p
+          key={player._id}>{player.name} has {player.score} point(s).
+          <button onClick={() => {
+            Players.update(player._id, {$inc: {score:-1}});}}>-1</button>
+          <button onClick={() => {
+            Players.update(player._id, {$inc: {score:1}});}}>+1</button>
+          <button onClick={() => Players.remove(player._id)}>X</button>
+      </p>
+    );
+  });
+};
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+const handleSubmit = (e) => {
+  let playerName = e.target.playerName.value;
+  e.preventDefault();
+  if (playerName) {
+    e.target.playerName.value = '';
+    Players.insert({
+      name: playerName,
+      score: 0
+    });
+  }
+};
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let name = 'Mike';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is my second p.</p>
+        {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player name"/>
+          <button>Add Player</button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
 });
